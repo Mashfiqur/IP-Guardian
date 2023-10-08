38c8c3423c2955640fc48a1fab0f8044cd0d28ff
Mashfiq <mashfiqurrr@gmail.com>
2023-10-08 19:23:35 +0600

    feat: Setup frontend with Vue Js, Tailwind Css, Vue Router, Pinia
    In this commit, I have set up the frontend using Vue.js as the primary framework, along with several essential tools to enhance the development process and user experience.
    
    Here are the key components of this setup:
    
    1. *Vue Js:* Vue Js as frontend framework
    
    2. *Vue Router:* Vue Router to enable seamless navigation
    
    3. *Tailwind CSS:* Tailwind CSS as utility-first CSS framework
    
    4. *Pinia:* Pinia as a state management library

---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-zinc-600">Welcome</h1>
+</template>
+<script setup>
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import '../css/app.css';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import App from '@src/App.vue'
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia)
+    .mount('#app')
